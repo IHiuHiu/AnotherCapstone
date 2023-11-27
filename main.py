2132de13e672ab6dc1faa34c6449ab694d36e1d2
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-11-27 15:11:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -269,9 +269,7 @@ getSeverityDict()
 getDescription()
 getprecautionDict()
 
-        
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
