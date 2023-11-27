d219ddf0255b03f2b462989e566ef9872be4a420
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-11-27 14:01:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-pip install git
-git clone https://github.com/itachi9604/healthcare-chatbot.git
-pip install -r /content/healthcare-chatbot/requirements.txt
-pip install streamlit
+!pip install git
+!git clone https://github.com/itachi9604/healthcare-chatbot.git
+!pip install -r /content/healthcare-chatbot/requirements.txt
+!pip install streamlit
 import re
 import pandas as pd
 import pyttsx3
