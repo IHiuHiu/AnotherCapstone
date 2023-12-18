6ba9e5458c778d8716c3474660f76f9fc9503e3b
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-12-18 22:06:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def tree_to_code(tree, feature_names):
             if st.session_state.count < len(st.session_state.symptoms_given):
                 question = "Are you experiencing any " + st.session_state.symptoms_given[int(st.session_state.count)] + " ?"
                 new_key = "symptom num "+ str(st.session_state.count)
-                if ans:=st.radio(question, key = new_key, options = ["yes" , "no"], value = "None"):
+                if ans:=st.radio(question, key = new_key, options = ["yes" , "no"]):
                     if not st.session_state:
                         st.stop()
                     else:
