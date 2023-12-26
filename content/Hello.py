7491d2d448c8950785cfa0e6f339f4b3c44eeec5
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-12-26 11:51:13 +0700

    Update Hello.py

--- content/Hello.py
+++ content/Hello.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Welcome",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Healthcare Application 👋")
+
+st.sidebar.success("What do you want to do?")
+
+st.markdown(
+    """
+    Introduction stuff blablabla
+"""
+)
