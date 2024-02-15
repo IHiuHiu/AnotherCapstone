a47f4018a2a690a3aa22e74bb360e7f42366e991
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2024-02-15 16:12:14 +0700

    Update Hello.py

--- content/Hello.py
+++ content/Hello.py
@@ -15,12 +15,17 @@ st.markdown(
 """
 )
 from shillelagh.backends.apsw.db import connect
-conn = connect('https://docs.google.com/spreadsheets/d/1m2SZgMap_UpqFDc9anr1Ac6hXPc9u65KZRLTHWZhZtk/edit?usp=sharing')
-result = conn.execute("""
+sheets_url='https://docs.google.com/spreadsheets/d/1m2SZgMap_UpqFDc9anr1Ac6hXPc9u65KZRLTHWZhZtk/edit#gid=0'
+csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+result1 = pd.read_csv(csv_url)
+conn = connect(csv_url)
+result2 = conn.execute("""
     SELECT
         *
     FROM
         "https://docs.google.com/spreadsheets/d/1m2SZgMap_UpqFDc9anr1Ac6hXPc9u65KZRLTHWZhZtk/edit?usp=sharing"
 """, headers=1)
-for row in result:
-    print(row)
+for row in result1:
+    print(row + '1')
+for row in result2:
+    print(row + '2')
