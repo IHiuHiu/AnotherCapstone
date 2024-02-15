eb25c91869707af3029c74fd4ab9e6213d870a2d
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2024-02-15 19:35:57 +0700

    Update Hello.py

--- content/Hello.py
+++ content/Hello.py
@@ -19,26 +19,164 @@ st.markdown(
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM userinfo;', ttl="10m")
+conn.query('\x on;')
+db = conn.query('SELECT * FROM userinfo;', ttl="10m")
 
 # Print results.
-for row in df.itertuples():
-    st.write(row)
+def insert_user(username, email, password):
+    date_joined = str(datetime.datetime.now())
+    conn.query('INSERT INTO userinfo (username, email, password, datejoin) VALUES ('+username+' ,'+email+' ,'+password+' ,'+date_joined+');')
+
+def fetch_userinfo():
+    """
+    Fetch Users
+    :return Dictionary of Users:
+    """
+    username = db.get('username')
+    email = db.get('email')
+    password = db.get('password')
+
+def get_user_emails():
+    """
+    Fetch User Emails
+    :return List of user emails:
+    """
+    users = db.fetch()
+    emails = []
+    for user in users.items:
+        emails.append(user['key'])
+    return emails
 
 
+def get_usernames():
+    """
+    Fetch Usernames
+    :return List of user usernames:
+    """
+    users = db.fetch()
+    usernames = []
+    for user in users.items:
+        usernames.append(user['key'])
+    return usernames
 
-#from shillelagh.backends.apsw.db import connect
-#sheets_url='https://docs.google.com/spreadsheets/d/1m2SZgMap_UpqFDc9anr1Ac6hXPc9u65KZRLTHWZhZtk/edit#gid=0'
-#csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-#result1 = pd.read_csv(csv_url)
-#conn = connect(sheets_url)
-#result2 = conn.execute("""
-#    SELECT
-#        *
-#    FROM
-#        "https://docs.google.com/spreadsheets/d/1m2SZgMap_UpqFDc9anr1Ac6hXPc9u65KZRLTHWZhZtk/edit?usp=sharing"
-#""", headers=1)
-#for  in pd.DataFrame(result1):
-#    st.write(row + '1')
-#for row in result2:
-#    print(row + '2')
+def validate_email(email):
+    """
+    Check Email Validity
+    :param email:
+    :return True if email is valid else False:
+    """
+    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$" #tesQQ12@gmail.com
+
+    if re.match(pattern, email):
+        return True
+    return False
+
+
+def validate_username(username):
+    """
+    Checks Validity of userName
+    :param username:
+    :return True if username is valid else False:
+    """
+
+    pattern = "^[a-zA-Z0-9]*$"
+    if re.match(pattern, username):
+        return True
+    return False
+
+
+def sign_up():
+    with st.form(key='signup', clear_on_submit=True):
+        st.subheader(':green[Sign Up]')
+        email = st.text_input(':blue[Email]', placeholder='Enter Your Email')
+        username = st.text_input(':blue[Username]', placeholder='Enter Your Username')
+        password1 = st.text_input(':blue[Password]', placeholder='Enter Your Password', type='password')
+        password2 = st.text_input(':blue[Confirm Password]', placeholder='Confirm Your Password', type='password')
+
+        if email:
+            if validate_email(email):
+                if email not in get_user_emails():
+                    if validate_username(username):
+                        if username not in get_usernames():
+                            if len(username) >= 2:
+                                if len(password1) >= 6:
+                                    if password1 == password2:
+                                        # Add User to DB
+                                        hashed_password = stauth.Hasher([password2]).generate()
+                                        insert_user(email, username, hashed_password[0])
+                                        st.success('Account created successfully!!')
+                                        st.balloons()
+                                    else:
+                                        st.warning('Passwords Do Not Match')
+                                else:
+                                    st.warning('Password is too Short')
+                            else:
+                                st.warning('Username Too short')
+                        else:
+                            st.warning('Username Already Exists')
+
+                    else:
+                        st.warning('Invalid Username')
+                else:
+                    st.warning('Email Already exists!!')
+            else:
+                st.warning('Invalid Email')
+
+        btn1, bt2, btn3, btn4, btn5 = st.columns(5)
+
+        with btn3:
+            st.form_submit_button('Sign Up')
+
+try:
+    users = fetch_users()
+    emails = []
+    usernames = []
+    passwords = []
+
+    for user in users:
+        emails.append(user['key'])
+        usernames.append(user['username'])
+        passwords.append(user['password'])
+
+    credentials = {'usernames': {}}
+    for index in range(len(emails)):
+        credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
+
+    Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='abcdef', cookie_expiry_days=4)
+
+    email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
+
+    info, info1 = st.columns(2)
+
+    if not authentication_status:
+        sign_up()
+
+    if username:
+        if username in usernames:
+            if authentication_status:
+                # let User see app
+                st.sidebar.subheader(f'Welcome {username}')
+                Authenticator.logout('Log Out', 'sidebar')
+
+                st.subheader('This is the home page')
+                st.markdown(
+                    """
+                    ---
+                    Created with ❤️ by SnakeByte
+                    
+                    """
+                )
+
+            elif not authentication_status:
+                with info:
+                    st.error('Incorrect Password or username')
+            else:
+                with info:
+                    st.warning('Please feed in your credentials')
+        else:
+            with info:
+                st.warning('Username does not exist, Please Sign up')
+
+
+except:
+    st.success('Refresh Page')
