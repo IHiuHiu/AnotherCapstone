5caa9a1811da6f0ab3b54ae1afeb3f05169678cf
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2024-02-16 12:16:25 +0700

    Update Hello.py

--- content/Hello.py
+++ content/Hello.py
@@ -162,6 +162,7 @@ if username:
                 """
             )
             st.session_state['current_user'] = username
+            c_user = username
 
         elif not authentication_status:
             with info:
