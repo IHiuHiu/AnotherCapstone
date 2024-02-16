32c73bbe108dadeb0f936c7c497f960f3de60bbe
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2024-02-16 12:48:33 +0700

    Update Hello.py

--- content/Hello.py
+++ content/Hello.py
@@ -32,9 +32,9 @@ conn = st.connection("postgresql", type="sql")
 def insert_user(username, email, password):
     date_joined = datetime.datetime.now()
     date = str(date_joined.year) + '-' + str(date_joined.month) + '-' + str(date_joined.day)
-    conn.query('INSERT INTO userinfo (username, email, password, datejoin) VALUES ("' +username+'","'+email+'","'+password+'","'+date+'");')
+    conn.query(f"INSERT INTO userinfo (username, email, password, datejoin) VALUES ('{username}','{email}','{password}','{date}');")
 
-def get_user_emails():
+def get_user_emails():0
     """
     Fetch User Emails
     :return List of user emails:
