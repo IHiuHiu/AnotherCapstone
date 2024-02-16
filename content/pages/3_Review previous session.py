997e3915460520064130815c3591f216e4186e51
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2024-02-16 14:33:55 +0700

    Update 3_Review previous session.py

--- content/pages/3_Review previous session.py
+++ content/pages/3_Review previous session.py
@@ -1,1 +1,17 @@
-st.sidebar.subheader(f'Welcome {username}')
+import re
+import pandas as pd
+import pyttsx3
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier,_tree
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+import csv
+import warnings
+import streamlit as st
+import streamlit_authenticator as stauth
+import datetime
+import psycopg2
+from sqlalchemy import create_engine
+
